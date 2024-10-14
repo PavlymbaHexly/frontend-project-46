b0e08590d9fd0f7299dab18e2015f91d81a8ea44
Pavel <pavlymba@gmail.com>
2024-10-14 11:05:21 +0300

    lastovo

--- src/getTree.js
+++ src/getTree.js
@@ -1,6 +1,5 @@
 import _ from 'lodash';
 
-// Основная функция
 const getTree = (obj1, obj2) => {
   const keys = _.sortBy(_.uniq([...Object.keys(obj1), ...Object.keys(obj2)]));
 
@@ -10,7 +9,7 @@ const getTree = (obj1, obj2) => {
     const areBothObjects = _.isObject(obj1[key]) && _.isObject(obj2[key])
       && !Array.isArray(obj1[key]) && !Array.isArray(obj2[key]);
 
-    // Создаем новый объект для результата
+    // Создаем объект результата с использованием Object.assign для предотвращения переназначения
     const result = { key };
 
     // Условие для вложенных объектов
@@ -18,19 +17,15 @@ const getTree = (obj1, obj2) => {
       result.value = getTree(obj1[key], obj2[key]);
       result.type = 'nested';
     } else if (hasKeyInObj1 && hasKeyInObj2 && _.isEqual(obj1[key], obj2[key])) {
-      // Условие для одинаковых значений
       result.value = obj1[key];
       result.type = 'unchanged';
     } else if (!hasKeyInObj2) {
-      // Условие для удалённых ключей
       result.value = obj1[key];
       result.type = 'deleted';
     } else if (!hasKeyInObj1) {
-      // Условие для добавленных ключей
       result.value = obj2[key];
       result.type = 'added';
     } else {
-      // Условие для изменённых значений
       result.valueDeleted = obj1[key];
       result.valueAdded = obj2[key];
       result.type = 'changed';
