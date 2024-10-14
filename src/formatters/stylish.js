4cde06b2540af0a54341df97cb5b60f90d242843
Pavel <pavlymba@gmail.com>
2024-10-14 11:23:35 +0300

    lastovo

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -12,11 +12,13 @@ const stringify = (currentValue, depth) => {
   if (!_.isObject(currentValue)) {
     return String(currentValue);
   }
-  const lines = Object
-    .entries(currentValue)
-    .map(([key, value]) => `${getIndent(depth)}${signOfDiffer.unchanged}${key}: ${stringify(value, depth + 1)}`);
+  const lines = Object.entries(currentValue).map(
+    ([key, value]) => `${getIndent(depth)}${signOfDiffer.unchanged}${key}: ${stringify(value, depth + 1)}`,
+  );
   return ['{', ...lines, `${getIndent(depth, 4)}}`].join('\n');
 };
+
+const formatValue = (obj, depth) => `${getIndent(depth)}${signOfDiffer[obj.type]}${obj.key}: ${stringify(obj.value, depth + 1)}`;
 
 const stylish = (difTree) => {
   const getRender = (treeAST, depth = 1) => {
@@ -27,11 +29,11 @@ const stylish = (difTree) => {
         case 'unchanged':
         case 'deleted':
         case 'added':
-          return `${getIndent(depth)}${signOfDiffer[obj.type]}${obj.key}: ${stringify(obj.value, depth + 1)}`;
+          return formatValue(obj, depth);
         case 'changed': {
-          const deleteValue = `${getIndent(depth)}${signOfDiffer.deleted}${obj.key}: ${stringify(obj.valueDeleted, depth + 1)}\n`;
-          const addedValue = `${getIndent(depth)}${signOfDiffer.added}${obj.key}: ${stringify(obj.valueAdded, depth + 1)}`;
-          return `${deleteValue}${addedValue}`;
+          const deleteValue = formatValue({ ...obj, type: 'deleted', value: obj.valueDeleted }, depth);
+          const addedValue = formatValue({ ...obj, type: 'added', value: obj.valueAdded }, depth);
+          return `${deleteValue}\n${addedValue}`;
         }
         default:
           return '';
