807a06a0c67137f935e6f570d2f1f6b9b7fd3462
Pavel <pavlymba@gmail.com>
2024-10-14 11:02:01 +0300

    lastovo

--- src/getTree.js
+++ src/getTree.js
@@ -4,63 +4,33 @@ const getTree = (obj1, obj2) => {
   const keys = _.sortBy(_.uniq([...Object.keys(obj1), ...Object.keys(obj2)]));
 
   return keys.map((key) => {
-    const type = getType(key, obj1, obj2);
-    const value = getValue(key, obj1, obj2, type);
+    const hasKeyInObj1 = _.has(obj1, key);
+    const hasKeyInObj2 = _.has(obj2, key);
+    const areBothObjects = _.isObject(obj1[key]) && _.isObject(obj2[key])
+      && !Array.isArray(obj1[key]) && !Array.isArray(obj2[key]);
 
-    return {
-      key,
-      ...(typeof value === 'object' ? value : { value }),
-      type,
-    };
+    const result = { key };
+
+    if (hasKeyInObj1 && hasKeyInObj2 && areBothObjects) {
+      result.value = getTree(obj1[key], obj2[key]);
+      result.type = 'nested';
+    } else if (hasKeyInObj1 && hasKeyInObj2 && _.isEqual(obj1[key], obj2[key])) {
+      result.value = obj1[key];
+      result.type = 'unchanged';
+    } else if (!hasKeyInObj2) {
+      result.value = obj1[key];
+      result.type = 'deleted';
+    } else if (!hasKeyInObj1) {
+      result.value = obj2[key];
+      result.type = 'added';
+    } else {
+      result.valueDeleted = obj1[key];
+      result.valueAdded = obj2[key];
+      result.type = 'changed';
+    }
+
+    return result;
   });
-};
-
-const getType = (key, obj1, obj2) => {
-  const hasKeyInObj1 = _.has(obj1, key);
-  const hasKeyInObj2 = _.has(obj2, key);
-  const areBothObjects = _.isObject(obj1[key]) && _.isObject(obj2[key])
-    && !Array.isArray(obj1[key]) && !Array.isArray(obj2[key]);
-
-  if (hasKeyInObj1 && hasKeyInObj2 && areBothObjects) {
-    return 'nested';
-  }
-
-  if (hasKeyInObj1 && hasKeyInObj2 && _.isEqual(obj1[key], obj2[key])) {
-    return 'unchanged';
-  }
-
-  if (!hasKeyInObj2) {
-    return 'deleted';
-  }
-
-  if (!hasKeyInObj1) {
-    return 'added';
-  }
-
-  return 'changed';
-};
-
-const getValue = (key, obj1, obj2, type) => {
-  if (type === 'nested') {
-    return getTree(obj1[key], obj2[key]);
-  }
-
-  if (type === 'unchanged') {
-    return obj1[key];
-  }
-
-  if (type === 'deleted') {
-    return obj1[key];
-  }
-
-  if (type === 'added') {
-    return obj2[key];
-  }
-
-  return {
-    valueDeleted: obj1[key],
-    valueAdded: obj2[key],
-  };
 };
 
 export default getTree;
