d40767d64110260e8a1a4370c9f18ef20b050f9d
Pavel <pavlymba@gmail.com>
2024-10-14 10:56:23 +0300

    lastovo

--- src/getTree.js
+++ src/getTree.js
@@ -1,51 +1,64 @@
 import _ from 'lodash';
 
+const getType = (key, obj1, obj2) => {
+  const hasKeyInObj1 = _.has(obj1, key);
+  const hasKeyInObj2 = _.has(obj2, key);
+  const areBothObjects = _.isObject(obj1[key]) && _.isObject(obj2[key])
+    && !Array.isArray(obj1[key]) && !Array.isArray(obj2[key]);
+
+  if (hasKeyInObj1 && hasKeyInObj2 && areBothObjects) {
+    return 'nested';
+  }
+
+  if (hasKeyInObj1 && hasKeyInObj2 && _.isEqual(obj1[key], obj2[key])) {
+    return 'unchanged';
+  }
+
+  if (!hasKeyInObj2) {
+    return 'deleted';
+  }
+
+  if (!hasKeyInObj1) {
+    return 'added';
+  }
+
+  return 'changed';
+};
+
+const getValue = (key, obj1, obj2, type) => {
+  if (type === 'nested') {
+    return getTree(obj1[key], obj2[key]);
+  }
+
+  if (type === 'unchanged') {
+    return obj1[key];
+  }
+
+  if (type === 'deleted') {
+    return obj1[key];
+  }
+
+  if (type === 'added') {
+    return obj2[key];
+  }
+
+  return {
+    valueDeleted: obj1[key],
+    valueAdded: obj2[key],
+  };
+};
+
 const getTree = (obj1, obj2) => {
   const keys = _.sortBy(_.uniq([...Object.keys(obj1), ...Object.keys(obj2)]));
 
   return keys.map((key) => {
-    const hasKeyInObj1 = _.has(obj1, key);
-    const hasKeyInObj2 = _.has(obj2, key);
-    const areBothObjects = _.isObject(obj1[key]) && _.isObject(obj2[key])
-      && !Array.isArray(obj1[key]) && !Array.isArray(obj2[key]);
-
-    if (hasKeyInObj1 && hasKeyInObj2 && areBothObjects) {
-      return {
-        key,
-        value: getTree(obj1[key], obj2[key]),
-        type: 'nested',
-      };
-    }
-
-    if (hasKeyInObj1 && hasKeyInObj2 && _.isEqual(obj1[key], obj2[key])) {
-      return {
-        key,
-        value: obj1[key],
-        type: 'unchanged',
-      };
-    }
-
-    if (!hasKeyInObj2) {
-      return {
-        key,
-        value: obj1[key],
-        type: 'deleted',
-      };
-    }
-
-    if (!hasKeyInObj1) {
-      return {
-        key,
-        value: obj2[key],
-        type: 'added',
-      };
-    }
+    const type = getType(key, obj1, obj2);
+    const value = getValue(key, obj1, obj2, type);
 
     return {
       key,
-      valueDeleted: obj1[key],
-      valueAdded: obj2[key],
-      type: 'changed',
+      ...(typeof value === 'object' ? value : { value }),
+      type,
     };
   });
 };
